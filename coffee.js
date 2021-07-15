5837a5b2860bf914db169ef49b6102f02accadc1
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-07-14 21:18:50 -0500

    js update

--- coffee.js
+++ coffee.js
@@ -1,9 +1,14 @@
-var form= form.formName
 
-function getTotal(){
-    var cupcake= Number(formName.cupcake.value) * 2.50;
-    var latte= Number(FormName.latte.value) * 3;
-    var cookie= Number(FormName.cookie.value) * 1.50;
-    
-    document.getElementById("total")= cupcake + latte + cookie;
-}
+
+
+function Total(){
+    var tot = 0;
+    tot += f.cupcake.value * 2.5 +
+           f.latte.value * 3 +
+           f.cookie.value * 1.5;
+        
+    document.getElementById("cost").value = "total" + tot.toFixed(2);
+  }
+  
+  var f = document.forms['myForm'];
+  
